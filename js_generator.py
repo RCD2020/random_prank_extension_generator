cd3965d55c6ec945200261b71ca4b41924db0de9
RCD2020 <dobertravis@icloud.com>
2026-02-16 19:46:02 -0700

    added more actions

--- js_generator.py
+++ js_generator.py
@@ -25,6 +25,41 @@ ACTIONS = [
             'prank_blur_amt += .1;',
             'document.body.style.filter = `blur(${prank_blur_amt}px)`;'
         ])
+    },
+    {
+        'prep': 'let prank_brightness_amt = 100;',
+        'func': _function_inner_body([
+            'prank_brightness_amt -= 1;',
+            'document.body.style.filter = `brightness(${prank_brightness_amt}%)`;'
+        ])
+    },
+    {
+        'prep': 'let prank_brightness_amt = 100;',
+        'func': _function_inner_body([
+            'prank_brightness_amt += 1;',
+            'document.body.style.filter = `brightness(${prank_brightness_amt}%)`;'
+        ])
+    },
+    {
+        'prep': 'let prank_contrast_amt = 100;',
+        'func': _function_inner_body([
+            'prank_contrast_amt -= 1;',
+            'document.body.style.filter = `contrast(${prank_contrast_amt}%)`;'
+        ])
+    },
+    {
+        'prep': 'let prank_contrast_amt = 100;',
+        'func': _function_inner_body([
+            'prank_contrast_amt += 1;',
+            'document.body.style.filter = `contrast(${prank_contrast_amt}%)`;'
+        ])
+    },
+    {
+        'prep': 'let prank_hue_amt = 0;',
+        'func': _function_inner_body([
+            'prank_hue_amt += 4;',
+            'document.body.style.filter = `hue-rotate(${prank_hue_amt}deg)`;'
+        ])
     }
 ]
 
